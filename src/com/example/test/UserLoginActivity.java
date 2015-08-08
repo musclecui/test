b4a9674d564d58cea0fbbe87749156b48af01732
崔炎新 <yanxin.cui@nanhua.com.cn>
2015-08-08 13:18:20 +0800

    15-08-08 13:18

--- src/com/example/test/UserLoginActivity.java
+++ src/com/example/test/UserLoginActivity.java
@@ -4,12 +4,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class UserLoginActivity extends Activity {
 
@@ -30,20 +33,42 @@ public class UserLoginActivity extends Activity {
 				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // 设置日期格式
 				Date dt = new Date();
 				
-				EditText edUserName = (EditText)findViewById(R.id.edUserName);
-				EditText edPassword = (EditText)findViewById(R.id.edPassword);
-				String userName = edUserName.getText().toString();
-				String password = edPassword.getText().toString();
+				AlertDialog.Builder builder = new AlertDialog.Builder(UserLoginActivity.this);
+				builder.setTitle(R.string.app_name);
+				builder.setIcon(R.drawable.home);
+				builder.setMessage("你正在登录");
+				builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						Toast.makeText(UserLoginActivity.this, "你按了确定", Toast.LENGTH_LONG).show();
+					}
+				});
+				builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						Toast.makeText(UserLoginActivity.this, "你按了取消", Toast.LENGTH_LONG).show();
+					}
+				});
+				builder.create().show();
 				
-				String s = userName + password;
-				Intent intent = new Intent(UserLoginActivity.this, ToDetVehActivity.class);
-				Bundle bundle = new Bundle();
-				bundle.putString("1", "One");
-				bundle.putString("2", "Two");
-				bundle.putString("3", "Three");
-				bundle.putInt("4", 4);
-				intent.putExtras(bundle);
-				startActivity(intent);
+//				EditText edUserName = (EditText)findViewById(R.id.edUserName);
+//				EditText edPassword = (EditText)findViewById(R.id.edPassword);
+//				String userName = edUserName.getText().toString();
+//				String password = edPassword.getText().toString();
+//				
+//				String s = userName + password;
+//				Intent intent = new Intent(UserLoginActivity.this, ToDetVehActivity.class);
+//				Bundle bundle = new Bundle();
+//				bundle.putString("1", "One");
+//				bundle.putString("2", "Two");
+//				bundle.putString("3", "Three");
+//				bundle.putInt("4", 4);
+//				intent.putExtras(bundle);
+//				startActivity(intent);
 				
 //				finish();
 				
