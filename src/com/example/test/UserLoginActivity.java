3eb09994cce1a44deaa32f70f74e9ae1e2043de7
崔炎新 <yanxin.cui@nanhua.com.cn>
2015-08-11 17:32:57 +0800

    15-08-11 17:32

--- src/com/example/test/UserLoginActivity.java
+++ src/com/example/test/UserLoginActivity.java
@@ -4,15 +4,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class UserLoginActivity extends Activity {
 
@@ -30,36 +30,73 @@ public class UserLoginActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // 设置日期格式
-				Date dt = new Date();
+
+				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 				
-				AlertDialog.Builder builder = new AlertDialog.Builder(UserLoginActivity.this);
-				builder.setTitle(R.string.app_name);
-				builder.setIcon(R.drawable.home);
-				builder.setMessage("你正在登录");
-				builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+				LayoutInflater inflater = (LayoutInflater)UserLoginActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				final View vPopupWin = inflater.inflate(R.layout.dialog_popup, null, false);
+				final PopupWindow pw = new PopupWindow(vPopupWin, 400, 200, true);
+				
+				Button btnOk = (Button)vPopupWin.findViewById(R.id.btnOk);
+				Button btnCancel = (Button)vPopupWin.findViewById(R.id.btnCancel);
+				TextView tvTip = (TextView)vPopupWin.findViewById(R.id.tvTip);
+				tvTip.setText("test");
+				
+				btnOk.setOnClickListener(new OnClickListener() {
 					
 					@Override
-					public void onClick(DialogInterface dialog, int which) {
+					public void onClick(View v) {
 						// TODO Auto-generated method stub
-						Toast.makeText(UserLoginActivity.this, "你按了确定", Toast.LENGTH_LONG).show();
+
+						Date dt = new Date();
+						SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+						TextView tvTip = (TextView)vPopupWin.findViewById(R.id.tvTip);
+						tvTip.setText(df.format(dt));
+						
 					}
 				});
-				builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+				btnCancel.setOnClickListener(new OnClickListener() {
 					
 					@Override
-					public void onClick(DialogInterface dialog, int which) {
+					public void onClick(View v) {
 						// TODO Auto-generated method stub
-						Toast.makeText(UserLoginActivity.this, "你按了取消", Toast.LENGTH_LONG).show();
+						pw.dismiss();
 					}
 				});
-				builder.create().show();
+				pw.showAtLocation(v, Gravity.CENTER, 0, 0);
+				
+				
+//				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // 设置日期格式
+//				Date dt = new Date();
+				
+				
+//				AlertDialog.Builder builder = new AlertDialog.Builder(UserLoginActivity.this);
+//				builder.setTitle(R.string.app_name);
+//				builder.setIcon(R.drawable.home);
+//				builder.setMessage("你正在登录");
+//				builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+//					
+//					@Override
+//					public void onClick(DialogInterface dialog, int which) {
+//						// TODO Auto-generated method stub
+//						Toast.makeText(UserLoginActivity.this, "你按了确定", Toast.LENGTH_LONG).show();
+//					}
+//				});
+//				builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+//					
+//					@Override
+//					public void onClick(DialogInterface dialog, int which) {
+//						// TODO Auto-generated method stub
+//						Toast.makeText(UserLoginActivity.this, "你按了取消", Toast.LENGTH_LONG).show();
+//					}
+//				});
+//				builder.create().show();
+				
 				
 //				EditText edUserName = (EditText)findViewById(R.id.edUserName);
 //				EditText edPassword = (EditText)findViewById(R.id.edPassword);
 //				String userName = edUserName.getText().toString();
 //				String password = edPassword.getText().toString();
-//				
 //				String s = userName + password;
 //				Intent intent = new Intent(UserLoginActivity.this, ToDetVehActivity.class);
 //				Bundle bundle = new Bundle();
@@ -69,8 +106,8 @@ public class UserLoginActivity extends Activity {
 //				bundle.putInt("4", 4);
 //				intent.putExtras(bundle);
 //				startActivity(intent);
-				
 //				finish();
+				
 				
 //				int i = 1;
 //				float f = 2.2f;
@@ -86,5 +123,11 @@ public class UserLoginActivity extends Activity {
 //				tst.show();
 			}
 		});
+	}
+	
+	
+	@Override
+	protected boolean onCreateOptionMenu {
+		
 	}
 }
